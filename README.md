55b8448d3c6cae5381dbdb87c8d2ff90a753070b
mr664 <m.ravenhall@live.co.uk>
2017-09-05 07:17:29 +0100

    Converted basicNN to class structure, added basic documentation

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # BasicNeuralNetwork
 Just messing around with neural network fundamentals
+
+## Running basicNN
+
+1. Import module
+2. Create an instance of the SimpleNN class
+3. Train model on included training dataset, assign weights to w
+4. Pass these weights and test data to testModel
+
+```python
+>>> import basicNN as bNN
+>>> n = bNN.SimpleNN()
+>>> w = n.trainModel()
+>>> n.testModel([1,0,0],w)
+0.9999370101226006
+```
--- basicNN.py
+++ basicNN.py
@@ -1,36 +1,35 @@
 import numpy as np
 
-modelRan = False
+class SimpleNN:
+    trainingInputs = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
+    trainingOutput = np.array([[0,1,1,0]]).T
 
-def sigmoid(x):
-    # Converts weighted inputs to 0,1 scale
-    return(1 / (1 + np.exp(-x)))
+    def sigmoid(self, x):
+        # Converts weighted inputs to 0,1 scale
+        return(1 / (1 + np.exp(-x)))
 
-def sigDeriv(x):
-    # Take gradient of sigmoid position
-    # Extreme (near 0/1) changes are weighted less than mid (~0.5) changes
-    return(x * (1 - x))
+    def sigDeriv(self, x):
+        # Take gradient of sigmoid position
+        # Extreme (near 0/1) changes are weighted less than mid (~0.5) changes
+        return(x * (1 - x))
 
-trainingInputs = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
-trainingOutput = np.array([[0,1,1,0]]).T
+    def trainModel(self, trainIn=trainingInputs, trainOut=trainingOutput, seed=2017, eons=10000):
+        # Set seed for replication
+        np.random.seed(seed)
 
+        # Set initial weights as random
+        weights = 2 * np.random.random((3,1)) - 1 # 3 input nodes to 1 output node
 
-def trainModel(trainIn, trainOut, seed=2017, eons=10000):
-    # Set seed for replication
-    np.random.seed(seed)
+        # Loop through network
+        for _ in range(eons):
+            # Take input and apply weights
+            output = self.sigmoid(np.dot(trainIn, weights))
 
-    # Set initial weights as random
-    weights = 2 * np.random.random((3,1)) - 1 # 3 input nodes to 1 output node
+            # Update weights according to error, also null weights on 0 inputs
+            weights += np.dot(trainIn.T, (trainOut - output)*self.sigDeriv(output))
 
-    # Loop through network
-    for _ in range(eons):
-        # Take input and apply weights
-        output = sigmoid(np.dot(trainIn, weights))
+        return(weights)
 
-        # Update weights according to error, also null weights on 0 inputs
-        weights += np.dot(trainIn.T, (trainOut - output)*sigDeriv(output))
-
-    return(weights)
-
-def testModel(testIn, trainedWeights):
-    return(sigmoid(np.dot(testIn, trainedWeights)))
+    def testModel(self, testIn, trainedWeights):
+        testIn = np.array(testIn)
+        return(float(self.sigmoid(np.dot(testIn, trainedWeights))))
